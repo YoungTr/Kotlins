afe4cc828da78f754e9031b3a6f80ad13bcdea83
tangyan <tangyan1004@gmail.com>
2021-05-30 11:45:17 +0800

    c3

--- src/com/youngtr/kotlins/Main.kt
+++ src/com/youngtr/kotlins/Main.kt
@@ -6,4 +6,8 @@ fun main(args: Array<String>) {
     val name = if (args.size > 0) args[0] else "kotlin"
 
     println("Hello $name")
+
+    val lest = listOf("1", "2", "3")
+    println(lest.max())
+    println(lest.last())
 }
--- src/com/youngtr/kotlins/c3/Main3.kt
+++ src/com/youngtr/kotlins/c3/Main3.kt
@@ -7,4 +7,20 @@ fun main() {
     println(sb.lastChar)
     sb.lastChar = '!'
     println(sb.lastChar)
+
+    val filePath = "/User/yole/kotlin-book/chapter.doc"
+
+    parsePath(filePath)
+}
+
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+
+    println("fullName: $fullName")
+
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("Dir: $directory, name: $fileName, ext $extension")
 }
